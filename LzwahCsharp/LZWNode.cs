25f26fd959495dd7369403053594b4705a8fcb5f
jakub judas <jakub@judas.cz>
2017-09-17 22:54:18 +0200

    LZW Encoder

---
+++ LzwahCsharp/LZWNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LzwahCsharp
+{
+    class LZWNode
+    {
+        public byte value;
+        public long identifier;
+        Dictionary<byte, LZWNode> children;
+        public LZWNode(byte value, long identifier)
+        {
+            this.value = value;
+            this.identifier = identifier;
+            this.children = new Dictionary<byte, LZWNode>();
+        }
+        public void AddChild(LZWNode child)
+        {
+            this.children[child.value] = child;
+        }
+        public bool ContainsValue(byte value)
+        {
+            return this.children.ContainsKey(value);
+        }
+        public LZWNode GetChild(byte value)
+        {
+            return this.children[value];
+        }
+    }
+}
---
+++ LzwahCsharpTests/LZWTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LzwahCsharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LzwahCsharp.Tests
+{
+    class MockEncoder : IEncoder
+    {
+        public List<long> values;
+        public MockEncoder()
+        {
+            this.values = new List<long>();
+        }
+        public void Encode(long value)
+        {
+            values.Add(value);
+        }
+        public long Decode()
+        {
+            return values[values.Count()-1];
+        }
+    }
+    [TestClass()]
+    public class LZWTests
+    {
+        [TestMethod()]
+        public void EncodeTest()
+        {
+            MockEncoder encoder = new MockEncoder();
+            LZW lzw = new LZW(encoder);
+            lzw.Encode((42));
+            lzw.Encode((43));
+            lzw.Encode((42));
+            lzw.Encode((43));
+            lzw.Encode((44));
+            Assert.AreEqual(encoder.values[0], 42);
+            Assert.AreEqual(encoder.values[1], 43);
+            Assert.AreEqual(encoder.values[2], 256);
+
+
+        }
+    }
+}
