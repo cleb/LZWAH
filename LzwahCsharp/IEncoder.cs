b8d23665500825c6c3e361af3fcc5d6a268e615b
jakub judas <jakub@judas.cz>
2017-09-17 08:46:19 +0200

    Encoder interface

---
+++ LzwahCsharp/IEncoder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LzwahCsharp
+{
+    interface IEncoder
+    {
+        void Encode(Int64 value);
+        long Decode();
+    }
+}
